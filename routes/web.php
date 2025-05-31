0c5e9b9c3b67614cc4b3c7b4f83c2886b962f433
IvanTsumbek <maincumbek@gmail.com>
2025-05-31 19:49:51 +0300

    model

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {    return 'hello';});
 
-Route::get('/my_page', 'App\Http\Controllers\MyPlaceController@index');
-Route::get('/contact', 'App\Http\Controllers\ContactController@index');
-Route::get('/about', 'App\Http\Controllers\AboutController@index');
-Route::get('/serveces', 'App\Http\Controllers\ServecesController@index');
-Route::get('/support', 'App\Http\Controllers\SupportController@index');
-Route::get('/education', 'App\Http\Controllers\EducationController@index'); 
-Route::get('/our_partners', 'App\Http\Controllers\OurPartnersController@indexA');
+Route::get('/posts', 'App\Http\Controllers\PostController@index');
+Route::get('/test', 'App\Http\Controllers\TestController@index');
