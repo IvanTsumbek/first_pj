6e6edc2522b52f1776e827f3fbf49f5c5c7584e3
IvanTsumbek <maincumbek@gmail.com>
2025-06-01 14:47:05 +0300

    Методы чтения данных

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 class PostController extends Controller
 {
    public function index() {
-      $post = Post::find(1);
+      $post = Post::find(1); //обращаемся к модели и ее методу и READ с базы по номеру id
      dd($post);
    }
 }
