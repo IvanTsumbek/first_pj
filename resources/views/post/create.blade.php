3b3abc18717895adf592684cf2c7b1bfd2b9c7a9
IvanTsumbek <maincumbek@gmail.com>
2025-06-08 18:50:34 +0300

    модиф интерф по круд

--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -7,30 +7,42 @@
 
     <div class="form-group">
       <label for="title">Title</label>
-      <input type="text" name="title" class="form-control" id="title" placeholder="Title">
+      <input value="{{old('title') }}" type="text" name="title" class="form-control" id="title" placeholder="Title">
+      @error('title')
+      <p class="text-danger">{{ $message }}</p> <!--зарезервированная переменная для тайтл -->
+    @enderror
     </div>
     <div class="form-group">
+
       <label for="content">Content</label>
-      <textarea class="form-control" name="content" id="content" placeholder="Content"></textarea>
+      <textarea class="form-control" name="content" id="content" placeholder="Content">{{ old('content') }}</textarea>
+      @error('content')
+      <p class="text-danger">{{ $message }}</p>
+    @enderror
     </div>
     <div class="form-group">
       <label for="image">Image</label>
-      <input type="text" name="image" class="form-control" id="image" placeholder="Image">
+      <input type="text" value="{{ old('image') }}" name="image" class="form-control" id="image" placeholder="Image">
+      @error('image')
+      <p class="text-danger">{{ $message }}</p>
+    @enderror
     </div>
     <div>
       <label for="category">Category</label>
       <select class="form-select" id="category" name="category_id">
       @foreach ($categories as $category)
-      <option value="{{ $category->id }}">{{$category->title}}</option>
+      <option 
+      {{old('category_id') == $category->id ? 'selected' : ''}}
+      value="{{ $category->id }}">{{$category->title}}</option>
     @endforeach
       </select>
     </div>
     <div class="form-group">
       <label for="tags">Tags</label>
       <select multiple class="form-control" id="tags" name="tags[]">
-        @foreach ($tags as $tag)
+      @foreach ($tags as $tag)
       <option value="{{ $tag->id }}">{{$tag->title}}</option>
-          @endforeach
+    @endforeach
       </select>
     </div>
     <button type="submit" class="btn btn-primary">Create</button>
