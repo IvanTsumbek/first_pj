ed6056829fcf80da09d4169d3f928ab53426c56d
IvanTsumbek <maincumbek@gmail.com>
2025-06-11 20:07:42 +0300

    Filter

--- app/Http/Controllers/Post/IndexController.php
+++ app/Http/Controllers/Post/IndexController.php
@@ -2,15 +2,21 @@
 
 namespace App\Http\Controllers\Post;
 
-// use BaseController;
 use App\Models\Post;
+use App\Http\Filters\PostFilter;
+use App\Http\Requests\Post\FilterRequest;
 
 class IndexController extends BaseController
 {
-public function __invoke()
-{
-   $posts = Post::paginate(10);
-   // dd($posts);
-   return view('post.index', compact('posts'));
-}
+   public function __invoke(FilterRequest $request)
+   {
+
+
+
+      $data = $request->validated();
+
+      $filter = app()->make(PostFilter::class, ['queryParams' => array_filter($data)]);
+      $posts = Post::filter($filter)->paginate(10);
+      return view('post.index', compact('posts'));
+   }
 }
